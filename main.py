c15c5b5040dc740a3eea207919ec3116616c769b
Drăghici Andrei <35525357+Sekushy@users.noreply.github.com>
2019-08-20 15:33:40 +0300

    Added generate_csv and get_random_article() func

--- main.py
+++ main.py
@@ -1,9 +1,33 @@
-import requests
+import requests, random, csv
 from bs4 import BeautifulSoup
+import pandas as pd
 
-if __name__ == "__main__":
+
+def generate_csv():
     URL = 'https://en.wikipedia.org/wiki/Wikipedia:Unusual_articles'
     page = requests.get(URL).content
     soup = BeautifulSoup(page, "lxml")
     tables = soup.find_all("table", "wikitable")
-    print(tables[0].find_all('a'))
+
+    final_links = []
+    titles = []
+    for i in range(0, len(tables)):
+        links = tables[i].find_all('a')
+        for link in links:
+            final_links.append(link.get('href'))
+            titles.append(link.get('title'))
+    
+    df = pd.DataFrame()
+    df['Title'] = titles
+    df['Link'] = final_links
+    df.to_csv('Unusual_articles.csv',index=False)
+
+def get_random_article():
+    csvfile = (pd.read_csv('Unusual_articles.csv')).sample()
+    title = csvfile['Title'].values[0]
+    link = 'https://en.wikipedia.org' + csvfile['Link'].values[0]
+    return title, link
+
+if __name__ == "__main__":
+    title_of_article, link_of_article = get_random_article()
+    print('Your random article of the day about {}: {}'.format(title_of_article, link_of_article))
