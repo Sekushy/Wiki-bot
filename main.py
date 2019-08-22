4e68fb1a361ae880e512849c610beefb42d7237d
Drăghici Andrei <35525357+Sekushy@users.noreply.github.com>
2019-08-22 17:22:55 +0300

    Created specialized functions

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import requests, random, csv, re
+import requests, random, csv
 from bs4 import BeautifulSoup
 import pandas as pd
-
 
 def generate_csv():
     URL = 'https://en.wikipedia.org/wiki/Wikipedia:Unusual_articles'
@@ -11,15 +10,12 @@ def generate_csv():
 
     final_links = []
     titles = []
-
     for i in range(0, len(tables)):
-        table_rows = tables[i].find_all('b')
-        for i in range(len(table_rows)):
-            links = table_rows[i].find_all('a')
-            for link in links:
-                final_links.append(link.get('href'))
-                titles.append(link.get('title'))
-
+        links = tables[i].find_all('a')
+        for link in links:
+            final_links.append(link.get('href'))
+            titles.append(link.get('title'))
+    
     df = pd.DataFrame()
     df['Title'] = titles
     df['Link'] = final_links
@@ -32,6 +28,6 @@ def get_random_article():
     return title, link
 
 if __name__ == "__main__":
-    #generate_csv()
+    generate_csv()
     title_of_article, link_of_article = get_random_article()
     print('Your random article of the day about {}: {}'.format(title_of_article, link_of_article))
